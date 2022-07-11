2f9560a1f5e566697451449df39f7504ffd4e001
J14032016 <bruce_jiang@163.com>
2022-07-11 15:17:50 +0800

    test2加了一段程序

--- test.py
+++ test.py
@@ -41,15 +41,3 @@ class Net(nn.Module):
 
 net = Net()
 print(net)
-<<<<<<< HEAD
-
-
-def num_flat_features(self, x):
-    size = x.size()[1:]  # all dimensions except the batch dimension
-    num_features = 1
-    for s in size:
-        num_features *= s
-    return num_features
-=======
-print('test')
->>>>>>> github/master
